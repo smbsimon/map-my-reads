8ca5fad6603533e584c9da7416330950a522ee5f
Sara Simon <smbsimon@gmail.com>
2014-11-17 18:51:08 -0700

    integrate twitter oauth

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.image = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_secret = auth.credentials.secret
+      user.save!
+    end
+  end
+
+end
