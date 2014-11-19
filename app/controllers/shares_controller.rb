b724bb6d4ac66d14a59bac89a983c5ff6605b7a9
Sara Simon <smbsimon@gmail.com>
2014-11-19 16:01:06 -0700

    styling for field tags

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,16 +1,5 @@
 class SharesController < ApplicationController
   def index
 
-  #   client = Twitter::REST::Client.new do |config|
-  #    config.consumer_key        = ENV['API_KEY']
-  #    config.consumer_secret     = ENV['API_SECRET']
-  #    config.access_token        = ENV['ACCESS_TOKEN']
-  #    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-  #   end
-  #
-  #   @tweet = "#{params[:book]} #{params[:location]}"
-  #
-  #   client.update(@tweet)
-  #
   end
 end
