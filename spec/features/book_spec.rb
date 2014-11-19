5e6d0634ecc16a96dd119cba67656a72cb3da8b2
Sara Simon <smbsimon@gmail.com>
2014-11-19 13:02:54 -0700

    blank slate for starting spec

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -3,14 +3,4 @@ require 'rails_helper'
 feature 'user visits book path' do
 
 
-
-  # let(:wish) { Wish.create(title: 'Win the lottery', description: 'Lorem ipsum dolor…') }
-
-  scenario 'user can move on to next page' do
-    visit book_path
-    expect(page).to have_css '.page_header'
-
-    # change this!!!!
-  end
-
 end
