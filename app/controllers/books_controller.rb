e97c61acbee702584b3982e78cb784872e36b232
Sara Simon <smbsimon@gmail.com>
2014-11-23 20:13:56 -0700

    set up book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,8 @@
 class BooksController < ApplicationController
   def new
   end
+
+  def index
+    @books = Book.all
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117234514) do
+ActiveRecord::Schema.define(version: 20141118175832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider"
