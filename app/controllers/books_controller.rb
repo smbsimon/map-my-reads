df529951980c228d241d86a1826bff92aa7eaa91
Sara Simon <smbsimon@gmail.com>
2014-11-23 20:50:13 -0700

    WIP part two database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def show
     @books = Book.all
+  end
+
+  def create
+    redirect_to new_location_path
   end
 end
