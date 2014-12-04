9c4c35301361aa737e99737dc55396e432af85ef
Sara Simon <smbsimon@gmail.com>
2014-12-03 21:33:59 -0700

    spec changes

--- spec/features/share_spec.rb
+++ spec/features/share_spec.rb
@@ -4,7 +4,7 @@ describe 'tweet', type: :feature do
 
   context 'when logged in' do
     before(:each) do
-      user = User.create({provider: "twitter", uid: 4, name: "Salmon", image: "test.png" oauth_token: "token", oauth_secret: "secret" })
+      user = User.create({provider: "twitter", uid: 4, name: "Salmon", image: "test.png", oauth_token: "token", oauth_secret: "secret" })
       log_in(user)
     end
 
