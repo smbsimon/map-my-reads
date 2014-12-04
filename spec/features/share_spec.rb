046de5b3b776bddb570826b34bf8e8d13998dfce
Sara Simon <smbsimon@gmail.com>
2014-12-03 21:30:42 -0700

    clean up shares test

--- spec/features/share_spec.rb
+++ spec/features/share_spec.rb
@@ -4,20 +4,13 @@ describe 'tweet', type: :feature do
 
   context 'when logged in' do
     before(:each) do
-      user = User.create({provider: "twitter", uid: 4, name: "Salmon", oauth_token: "token", oauth_secret: "secret" })
+      user = User.create({provider: "twitter", uid: 4, name: "Salmon", image: "test.png" oauth_token: "token", oauth_secret: "secret" })
       log_in(user)
     end
 
     xit 'can tweet' do
-      visit('/books/new')
-      fill_in('book[title]', :with => 'book title')
-      click_button('Create Book')
-      visit('/locations/new')
-      fill_in('location', :with => 'location title')
-      click_button('Next -->')
-      expect(page).to have_content('Share your book and location.')
+      visit('/shares/new')
       expect(page).to have_link('Tweet')
-      expect(page).to have_link('Log Out')
     end
   end
 
